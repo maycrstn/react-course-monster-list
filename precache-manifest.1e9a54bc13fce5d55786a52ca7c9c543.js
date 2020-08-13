815b98e7ec7e7d8990d2e2c300c752633cf7cadb
maycrstn <maycrstn@gmail.com>
2020-08-13 20:28:39 -0300

    Updates

---
+++ precache-manifest.1e9a54bc13fce5d55786a52ca7c9c543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9304f98cbc2b9fa03df4d09dc38ffab3",
+    "url": "/react-course-monster-list/index.html"
+  },
+  {
+    "revision": "dcf361beabfbd6ba234c",
+    "url": "/react-course-monster-list/static/css/main.388d6a9a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-course-monster-list/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-monster-list/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf361beabfbd6ba234c",
+    "url": "/react-course-monster-list/static/js/main.61e036d2.chunk.js"
+  },
+  {
+    "revision": "40627b16dd42fa835ba6",
+    "url": "/react-course-monster-list/static/js/runtime-main.fb3972f6.js"
+  }
+]);
